e4e9a1fa01c453576fb78a5b50da1f899440fb59
nmusi <n.musinov@gmail.com>
2021-05-11 16:37:14 +0300

    hw1

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -1,4 +1,3 @@
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,7 +6,7 @@ import java.io.IOException;
 public class FirstServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         for (int i = 1; i < 11; i++) {
             int x = (int) (Math.random() * 20);
           Product prod = new Product(i, "product " + x, x * 5);
